649ec886da58651f359710f072c9179eeaf63ca2
lawzeem <lawzeeml@buffalo.edu>
2022-06-01 23:12:17 -0500

    Updated the config command
    Made the config more user friendly.

--- vault.py
+++ vault.py
@@ -23,6 +23,7 @@ def loadUser(username):
     return details
 
 # Encrypt the username and password using the master credentials
+# Encoding is done using base64
 def encryptService(service, username, password, masterusername, masterpass):
     if(len(masterpass) != 16 or len(masterusername) != 16):
         print("Master Username and Password must be exactly 16 characters!")
@@ -49,6 +50,7 @@ def encryptService(service, username, password, masterusername, masterpass):
     return True
 
 # Decrypt the username and password for an account using master credentials
+# Master credentials were created during configuration
 def decryptService(account, masterusername, masterpass):
     try:
         aes_obj = AES.new(masterusername.encode("utf8"), AES.MODE_CFB, masterpass.encode("utf8"))
@@ -86,15 +88,19 @@ def retrieve(service, masterusername, masterpass):
 # Get all the services saved under the user
 def all(username):
     accounts = loadUser(username)
+    count = 0
     for key, value in accounts.items():
+        count+=1
         print(key)
+    if count == 0:
+        print("No accounts currently in Vault. To add an account, use the 'add' command")
 
 # Returns the number of accounts under the user
 def count(usesrname):
     accounts = loadUser(username)
     return len(accounts)
 
-# This function gets the config
+# This function gets the config file by reading the json
 def getConfig():
     config = {}
     exists = False
@@ -108,21 +114,33 @@ def getConfig():
     file.close()
     return exists, config
 
-# This function creates a config file
+# This function creates a config file as a json
+# Username and Password have to be 16 characters
+# Password uses the "getpass" which is similar to python input() but invisible for users in terminal
+# Password is Salted and Hashed and dumped in the config file with the Username
 def createConfig():
     config = {}
     filename = "config.json"
     vault_user = vault_pass1 = vault_pass2 = ""
     while(len(vault_user)!=16):
-        vault_user = input("Vault Username:")
+        vault_user = input("Vault Username: ")
+        # IF EXIT keyword is used while configuration, stop the program.
+        if vault_user == "EXIT":
+            sys.exit("Exiting the configuration.")
         if(len(vault_user)!=16):
             print("Username must be exactly 16 characters!")
     while(len(vault_pass1)!=16):
-        vault_pass1 = getpass("Vault Password:")
+        vault_pass1 = getpass("Vault Password: ")
+        # IF EXIT keyword is used while configuration, stop the program.
+        if vault_pass1 == "EXIT":
+            sys.exit("Exiting the configuration.")
         if(len(vault_pass1)!=16):
             print("Password must be exactly 16 characters!")
     while(vault_pass2!=vault_pass1):
-        vault_pass2 = getpass("Vault Password (again):")
+        vault_pass2 = getpass("Vault Password (again): ")
+        # IF EXIT keyword is used while configuration, stop the program.
+        if vault_pass2 == "EXIT":
+            sys.exit("Exiting the configuration.")
         if(vault_pass2!=vault_pass1):
             print("Passwords must match!")
     config["username"] = vault_user
@@ -264,19 +282,19 @@ if __name__ == "__main__":
     # Opener
     ascii_banner = pyfiglet.figlet_format("Vault")
     welcome_msg = "Welcome to Vault, this service encrypts user accounts and securely stores them.\nTo continue, please enter your username and password\n"
-    help_msg = "add\t\t\tAdd an account\nadd <account>\t\tAdd the account in the argument\ndecrypt\t\t\tDecrypt an account\ndecrypt <account>\tDecrypt the account in the argument\nedit\t\t\tUpdate an account\nedit <account>\t\tUpdate the account in the argument\ndelete\t\t\tDelete an account\ndelete <account>\tDelete the account in the argument\nall\t\t\tView all accounts\nexit\t\t\tExit vault"
+    help_msg = "\nadd\t\t\tAdd an account\nadd <account>\t\tAdd the account in the argument\ndecrypt\t\t\tDecrypt an account\ndecrypt <account>\tDecrypt the account in the argument\nedit\t\t\tUpdate an account\nedit <account>\t\tUpdate the account in the argument\ndelete\t\t\tDelete an account\ndelete <account>\tDelete the account in the argument\nall\t\t\tView all accounts\nexit\t\t\tExit vault\n"
     print(ascii_banner)
     # Checking if a config exists the directory and gets it
     config_exist, config_file = getConfig()
 
     if(config_exist):
-        print("Configuration found, enter password to continue")
+        print("Welcome to Vault! Please enter your password to continue")
         vault_pass = getUserDet()
         vault_user = config_file["username"]
         authenticate(config_file, vault_pass)
         runCommand(vault_user, vault_pass)
     else:
         # Since config does not exist, we make one and then run commands
-        print("No configuration found, create one to continue")
+        print("Welcome to Vault! Create a Username and Password to begin.\nUsername and Password must be 16 characters long.\nTo cancel configuration, type EXIT.\n")
         vault_user, vault_pass = createConfig()
         runCommand(vault_user, vault_pass)
